22c2ac61ad2d88de490b1cd7dee79af0266d4001
Paper Boii <mosesmafengcharles@gmail.com>
2026-03-14 08:20:49 +0100

    Add configuration for API and trading parameters

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+BASE_URL = "https://api-testnet.bybit.com"
+SYMBOL = "BTCUSDT"
+CATEGORY = "linear"
+TIMEFRAME = "15"
