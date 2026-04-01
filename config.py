0e4db98e10b8663048af37d0dcca49bd904e322c
Paper Boii <mosesmafengcharles@gmail.com>
2026-04-01 11:20:45 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
-API_KEY = os.getenv("API_KEY")
-API_SECRET = os.getenv("API_SECRET")
+API_KEY = os.environ.get("API_KEY")
+API_SECRET = os.environ.get("API_SECRET")
+TEST_MODE = os.environ.get("TEST_MODE", "True") == "True" 
 BASE_URL = "https://api-testnet.bybit.com"
 SYMBOL = "BTCUSDT"
 CATEGORY = "linear"
